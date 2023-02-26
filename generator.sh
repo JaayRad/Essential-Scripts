8529c7ed40e3fecfb04e23f95519351a12e753cb
JaayRad <zakubrad@gmail.com>
2023-02-26 16:59:58 +0100

    Intial/Scripting

---
+++ generator.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+ilosc_linii=20
+
+for((i=1; $i <= $ilosc_linii; i++))
+do
+	ilosc_liczb=$(($RANDOM & 7))
+	#echo $ilosc_liczb
+	j=1
+	while [ $j -le $ilosc_liczb  ]
+	do
+		echo -n $RANDOM >> liczby.txt
+		echo -n ' ' >> liczby.txt
+		j=$(($j+1))
+	done
+	echo >> liczby.txt
+
+done
+
